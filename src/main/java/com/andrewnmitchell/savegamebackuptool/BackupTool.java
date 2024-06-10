bde5abbba072c5e728831e52b070131dddde2789
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2024-06-10 13:34:46 -0500

    remove whitespace

--- src/main/java/com/andrewnmitchell/savegamebackuptool/BackupTool.java
+++ src/main/java/com/andrewnmitchell/savegamebackuptool/BackupTool.java
@@ -1,7 +1,6 @@
 package com.andrewnmitchell.savegamebackuptool;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.Gson;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
